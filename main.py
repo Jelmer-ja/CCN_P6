fa046356da6dfe2b3e3c3d443d576f968dbd456a
Jelmer-ja <jelmer-ja@hotmail.com>
2017-11-17 10:33:17 +0100

    added networks

--- main.py
+++ main.py
@@ -8,9 +8,16 @@ import chainer.functions as F
 import chainer.links as L
 import matplotlib.pyplot as plt
 from ANN import *
+from utils import *
 
 def main():
-    pass
+    epoch = 20
+    batch_size = 32
+    train_data, test_data = get_mnist(n_train=1000,n_test=100,with_label=False,classes=[0])
+    gen = Generator(784)
+    dis = Discriminator(784)
+    gen = GeneratorDistribution(784)
+    
 
 def run_network():
     pass
