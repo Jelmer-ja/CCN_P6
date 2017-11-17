1b8a2141e95b00bd9abbc5e24fd7b6a24a826378
Jelmer-ja <jelmer-ja@hotmail.com>
2017-11-17 09:37:05 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import numpy as np
+import chainer
+from chainer import cuda, Function, gradient_check, report, training, utils, Variable
+from chainer import datasets, iterators, optimizers, serializers
+from chainer import Link, Chain, ChainList
+from chainer.training import extensions
+import chainer.functions as F
+import chainer.links as L
+import matplotlib.pyplot as plt
+from RNN import *
+
+def main():
+    pass
+
+def run_network():
+    pass
+
+if(__name__ == "__main__"):
+    main()
