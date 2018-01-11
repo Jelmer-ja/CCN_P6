0098fa41f2e4e1772868bcfdefa2f07e098118a7
Jelmer-ja <jelmer-ja@hotmail.com>
2018-01-11 16:03:48 +0100

    k

--- ANN.py
+++ ANN.py
@@ -1,22 +1,27 @@
 import chainer.functions as F
-import chainer
-import numpy as np
 import chainer.links as L
-from chainer import Link, Chain, ChainList, report
+from chainer import Link, Chain, ChainList, report, iterators, optimizers
+import matplotlib.pyplot as plt
+import math
+import random
+from utils import *
 
 class Generator(Chain):
     def __init__(self, n_units):
         super(Generator, self).__init__()
+        self.mnist_dim = 28
+        self.n_units = n_units
         with self.init_scope():
             # the size of the inputs to each layer will be inferred
-            self.l1 = L.Linear(None, n_units)  # n_in -> n_units        INPUT LAYER
-            self.l2 = L.BatchNormalization(None, n_units)    # n_units -> n_out       BATCH NORMALIZATION
-            self.l3 = L.Deconvolution2D(None, 784)                   #  DECONVOLUTION
+            self.l1 = L.Linear(n_units * self.mnist_dim **2)  # n_in -> n_units        INPUT LAYER
+            self.l2 = L.BatchNormalization(n_units * self.mnist_dim ** 2)    # n_units -> n_out       BATCH NORMALIZATION
+            self.l3 = L.Deconvolution2D(in_channels=n_units, out_channels=1, ksize=3, stride=1,pad=1,outsize=(28,28))                   #  DECONVOLUTION
 
     def __call__(self, x):
-        h1 = F.relu(self.l1(x))
+        h1 = self.l1(x)
         h2 = F.relu(self.l2(h1))
-        y = F.sigmoid(self.l3(h2))
+        h = F.reshape(h2, [-1,self.n_units,self.mnist_dim,self.mnist_dim])
+        y = F.sigmoid(self.l3(h))
         return y
 
 class Discriminator(Chain):
@@ -24,15 +29,12 @@ class Discriminator(Chain):
         super(Discriminator, self).__init__()
         with self.init_scope():
             # the size of the inputs to each layer will be inferred
-            self.l1 = L.Linear(None, n_units)  # n_in -> n_units            INPUT LAYER
-            self.l2 = L.Convolution2D(None, n_units)  # n_units -> n_units  CONVOLUTIONAL LAYER
+            self.l2 = L.Convolution2D(in_channels=None, out_channels=n_units,ksize=3,stride=1)  # n_units -> n_units  CONVOLUTIONAL LAYER
             self.l3 = L.Linear(None, 1)    # n_units -> n_out
 
     def __call__(self, x):
-        h1 = F.relu(self.l1(x))
-        h2 = F.relu(self.l2(h1))
-        h3 = F.max_pooling_2d(h2)
-        y = F.relu(self.l3(h3))
+        h2 = F.relu(self.l2(x))
+        y = F.squeeze(self.l3(h2))
         return y
 
 class GeneratorDistribution(object):
--- main.py
+++ main.py
@@ -1,65 +1,52 @@
-import numpy as np
-import chainer
-from chainer import cuda, Function, gradient_check, report, training, utils, Variable
-from chainer import datasets, iterators, optimizers, serializers
-from chainer import Link, Chain, ChainList
-from chainer.training import extensions
 import chainer.functions as F
 import chainer.links as L
+from chainer import Link, Chain, ChainList, report, iterators, optimizers
 import matplotlib.pyplot as plt
 import math
 import random
-from ANN import *
 from utils import *
+from ANN import *
 
 def main():
-    epoch = 100
-    train_data, test_data = get_mnist(n_train=1000,n_test=100,with_label=False,classes=[0])
-    batch_size = 32
+    epoch = 2000
+    train_data, test_data = get_mnist(n_train=1000, n_test=100, with_label=False, classes=[0])
+    batch_size = 5
     n_units = 10
     gen = Generator(n_units)
     dis = Discriminator(n_units)
-    iterator = RandomIterator(train_data, batch_size=batch_size)
-    tester = RandomIterator(test_data, batch_size=batch_size)
-    g_optimizer = optimizers.SGD()
+    iterator = iterators.SerialIterator(train_data, batch_size)
+    g_optimizer = optimizers.Adam()
     g_optimizer.setup(gen)
-    d_optimizer = optimizers.SGD()
+    d_optimizer = optimizers.Adam()
     d_optimizer.setup(dis)
-    loss = run_network(epoch,batch_size,gen,dis,iterator,n_units,g_optimizer,d_optimizer)
-    showImages(gen,batch_size)
-    plot_loss(loss,epoch)
+    showtrain(train_data)
+    #loss = run_network(epoch, batch_size, gen, dis, iterator, n_units, g_optimizer, d_optimizer)
+    #showImages(gen)
+    #plot_loss(loss, epoch)
 
-def run_network(epoch,batch_size,gen,dis,iterator,n_units,g_optimizer,d_optimizer):
-    losses = [[],[]]
-    for i in range(0,epoch):
-        #for j in range (0,batch_size) THEY USED K=1 IN THE PAPER SO SO DO WE
-        k = 0
+def run_network(epoch, batch_size, gen, dis, iterator, n_units, g_optimizer, d_optimizer):
+    losses = [[], []]
+    for i in range(0, epoch):
+        # for j in range (0,batch_size) THEY USED K=1 IN THE PAPER SO SO DO WE
 
-        for batch in iterator:
-            k += 1
-            dis.cleargrads(); gen.cleargrads()
-            noise = randomsample(784,batch_size)
-            g_sample = gen(noise)
-            disc_gen = dis(g_sample)
-            disc_data = dis(np.reshape(batch,(batch_size,1,28,28),order='F'))
-            softmax1 = F.sigmoid_cross_entropy(disc_gen,np.zeros(batch_size).astype('int32'))
-            softmax2 = F.sigmoid_cross_entropy(disc_data,np.ones(batch_size).astype('int32'))
-            loss = softmax1 + softmax2
-            loss.backward()
-            d_optimizer.update()
-            losses[0].append(loss.data)
-            if(k >= 1):
-                break
-
-        noise = randomsample(784, batch_size)
-        gn = gen(noise)
-        if(i % 50 == 0):
-            plt.imshow(np.reshape(gn[0].data[:, ], (28, 28), order='F'))
-            plt.show()
-        loss = F.sigmoid_cross_entropy(dis(gn),np.ones(batch_size).astype('int32'))
+        batch = iterator.next()
+        dis.cleargrads();
+        gen.cleargrads()
+        noise = randomsample(20, batch_size)
+        g_sample = gen(noise)
+        disc_gen = dis(g_sample)
+        disc_data = dis(np.reshape(batch, (batch_size, 1, 28, 28), order='F'))
+        softmax1 = F.sigmoid_cross_entropy(disc_gen, np.zeros(batch_size).astype('int32'))
+        softmax2 = F.sigmoid_cross_entropy(disc_data, np.ones(batch_size).astype('int32'))
+        loss = softmax1 + softmax2
         loss.backward()
+        d_optimizer.update()
+        losses[0].append(loss.data)
+
+        gloss = F.sigmoid_cross_entropy(disc_gen, np.ones(batch_size).astype('int32'))
+        gloss.backward()
         g_optimizer.update()
-        losses[1].append(loss.data)
+        losses[1].append(gloss.data)
     return losses
 
 def randomsample(size,batch_size):
@@ -71,13 +58,30 @@ def plot_loss(loss,epoch):
     plt.legend()
     plt.show()
 
-def showImages(gen,batch_size):
-    batch_size = 1
-    noise = randomsample(784, batch_size)
-    images = gen(noise)
-    for i in images:
-        plt.imshow(np.reshape(i.data[:,], (28, 28), order='F'))
-        plt.show()
+def showImages(gen):
+    f,axes = plt.subplots(2,5)
+    for i in range(0,10):
+        if(i % 2 == 0):
+            x = 0
+        else:
+            x = 1
+        y = int(round(i/2,0))
+        noise = randomsample(20, 1)
+        image = gen(noise)
+        axes[x][y].imshow(np.reshape(image.data[:,], (28, 28), order='F'))
+    plt.show()
+
+def showtrain(train):
+    f,axes = plt.subplots(2,5)
+    for i in range(0,10):
+        if (i % 2 == 0):
+            x = 0
+        else:
+            x = 1
+        y = int(round(i / 2, 0))
+        image = train[i]
+        axes[x][y].imshow(np.reshape(image, (28, 28), order='F'))
+    plt.show()
 
 if(__name__ == "__main__"):
     main()
